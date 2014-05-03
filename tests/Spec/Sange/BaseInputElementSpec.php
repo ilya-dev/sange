bbb55af8794c65e156426851e0a279a99acb6137
Ilya S <public.ilya.s@gmail.com>
2014-05-03 22:09:45 +0700

    testing abstracts is non trivial

---
+++ tests/Spec/Sange/BaseInputElementSpec.php
@@ -0,0 +1,32 @@
+<?php namespace Spec\Sange;
+
+use PhpSpec\ObjectBehavior;
+
+class BaseInputElementSpec extends ObjectBehavior {
+
+    function let()
+    {
+        $this->beConstructedWith('foo');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sange\BaseInputElement');
+    }
+
+    function it_returns_name_of_the_input_element()
+    {
+        $this->getName()->shouldBe('foo');
+    }
+
+    function it_returns_value_of_the_input_element()
+    {
+        $this->getValue()->shouldBe(null);
+    }
+
+}
+
+namespace Sange;
+
+class BaseInputElement extends InputElement {}
+
