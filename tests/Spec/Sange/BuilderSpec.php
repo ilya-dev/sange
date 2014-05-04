0554b6ac1666acfe6413cbb2a787084783b3ee66
Ilya S <public.ilya.s@gmail.com>
2014-05-04 21:38:54 +0700

    add Builder

---
+++ tests/Spec/Sange/BuilderSpec.php
@@ -0,0 +1,13 @@
+<?php namespace Spec\Sange;
+
+use PhpSpec\ObjectBehavior;
+
+class BuilderSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sange\Builder');
+    }
+
+}
+
