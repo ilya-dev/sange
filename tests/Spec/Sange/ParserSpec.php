a93e76b5fe32738e04ea49cffee12c1c62281ece
Ilya S <public.ilya.s@gmail.com>
2014-05-05 20:54:56 +0700

    add Parser

---
+++ tests/Spec/Sange/ParserSpec.php
@@ -0,0 +1,13 @@
+<?php namespace Spec\Sange;
+
+use PhpSpec\ObjectBehavior;
+
+class ParserSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sange\Parser');
+    }
+
+}
+
