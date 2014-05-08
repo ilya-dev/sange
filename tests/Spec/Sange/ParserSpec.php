5483b70a4b73c56a560dbecbdcd8f4e79234a420
Ilya S <public.ilya.s@gmail.com>
2014-05-08 22:21:09 +0700

    final fix for ParserSpec - improve test

--- tests/Spec/Sange/ParserSpec.php
+++ tests/Spec/Sange/ParserSpec.php
@@ -31,9 +31,9 @@ class ParserSpec extends ObjectBehavior {
         $command->add(new Option('n', '10'));
         $command->add(new Option('a'));
         $command->add(new Option('b'));
-        $command->add(new Option('c'));
+        $command->add(new Option('c', '5'));
 
-        $this->parse('foo -n10 -abc')->shouldBeLike($command);
+        $this->parse('foo -n10 -abc 5')->shouldBeLike($command);
 
         $command = new Command('foo');
         $command->add(new Option('bar', 'baz'));
