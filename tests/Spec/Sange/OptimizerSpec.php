57c508edad14387cd70e545aad19a2dcfe0af14e
Ilya S <public.ilya.s@gmail.com>
2014-05-08 22:22:11 +0700

    create optimizer

---
+++ tests/Spec/Sange/OptimizerSpec.php
@@ -0,0 +1,13 @@
+<?php namespace Spec\Sange;
+
+use PhpSpec\ObjectBehavior;
+
+class OptimizerSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sange\Optimizer');
+    }
+
+}
+
